7362ef24179a4d91fa061ea3775d24a151c405ae
kangle <kangle@fiberhome.com>
2022-08-15 17:52:18 +0800

    桶排序

--- bucket/bucket.py
+++ bucket/bucket.py
@@ -7,3 +7,29 @@
 """
 
 
+def bucket_sort(li, n=100, max_num=10000):
+    buckets = [[] for _ in range(n)]  # 创建桶
+    for var in li:
+        i = min(var // (max_num // n), n - 1)  # 把var加到桶里面
+        buckets[i].append(var)
+        # 保持桶内的顺序
+        for j in range(len(buckets[i]) - 1, 0, -1):
+            if buckets[i][j] < buckets[i][j - 1]:
+                buckets[i][j], buckets[i][j - 1] = buckets[i][j - 1], \
+                                                   buckets[i][j]
+            else:
+                break
+    sorted_li = []
+    for buc in buckets:
+        sorted_li.extend(buc)
+
+    return sorted_li
+
+
+import random
+
+li = [random.randint(0, 100) for i in range(100)]
+print(li)
+
+li = bucket_sort(li)
+print(li)
