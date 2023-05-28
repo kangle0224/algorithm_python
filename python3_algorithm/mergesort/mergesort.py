84ae031d9d55bc5a731a147c22e5ce2f36fd60f4
kangle <kangle@fiberhome.com>
2023-05-28 23:41:37 +0800

    无序链表

---
+++ python3_algorithm/mergesort/mergesort.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+归并排序
+不太会，需要再看
+"""
+
+
+def mergeSort(alist):
+    print("splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i += 1
+            k += 1
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
+    print("merge ", alist)
+
+
+b = [52, 26, 93, 17, 77, 31, 44, 55, 20]
+mergeSort(b)
